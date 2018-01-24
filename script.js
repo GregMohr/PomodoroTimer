33e1498c65dce3abb2ace587ef1dffedb8b19aa4
Greg Mohr <gregmohr2000@yahoo.com>
2018-01-24 14:28:03 -0800

    Added checkboxes

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ End work when the timer rings and put a checkmark on a piece of paper.[5]
 If you have fewer than four checkmarks, take a short break (3–5 minutes), then go to step 2.
 After four pomodoros, take a longer break (15–30 minutes), reset your checkmark count to zero, then go to step 1.
 
-img range 9px-1035px
+img range 8px-1035px
 */
 $(document).ready(function() {
 
