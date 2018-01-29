5c831199d633535144dea56ecad777ea62f4a5bb
Greg Mohr <gregmohr2000@yahoo.com>
2018-01-29 14:49:01 -0800

    TimePhase object refactor done and code cleanup

--- script.js
+++ script.js
@@ -1,10 +1,4 @@
 /*
-Decide on the task to be done.
-Set durations for the Pomodoro, Short and Long rests. Traditionally these are, in minutes,  25, 3-5, and 15-30 respectiviely)
-Work on the task.
-End work when the timer rings and a checkmark will be placed in the appropriate repetition box
-After four pomodoros, take a longer break (15–30 minutes), reset your checkmark count to zero, then go to step 1.
-
 x. Add pause
 x. Add time display change opposite to logo
 x. Add duration controls reset, unlocks dur controls//may be able to combo the resets to 1 method by sending a callback of thw rest of the functionality. unlock dur controls is part of both
@@ -19,87 +13,77 @@ x. only allow numbers within range, even when typed, in durations
 
 should I protect against page refresh??
 */
-var timerRunning = false,
-    timer,
-    logoHeight = $("#logo-black").height(),
-    started = false;
-
 $(document).ready(function() {
-
+    logoHeight = $("#logo-black").height();
+    timer = new PomodoroTimer();
     $("#timer").click(startStop);
     $("#pom-dur").on('keyup input change', updateTimeDisplay);
-    //create timer here or leave it global. Should allow for both reset to assigned in here
-
 });
 
 function startStop() {
-    //timerRunning = !timerRunning;
-    if (!started) {
-        console.log("starting timer");
-        //start (disable duration controls. reset re-enables) or resume??
-        $(".duration").attr("disabled", true);
-        timer = new PomodoroTimer();
-        // $("#reset").click(timer.reset);
-        timer.runPhase();
-        started = true;
+    if (timer.isStarted) {
+        if (!timer.isPaused) {
+            console.log("pausing");
+            timer.pause();
+        } else {
+            console.log("resuming");
+            timer.resume();
+        }
     } else {
-        //clearInterval(timer);
+        $(".duration").attr("disabled", true);
+        timer.isStarted = true;
+        timer.initPhases();
     }
 }
 
-function TimerPhase(chk, secondsRemaining, logo, action, endPhase) {
-    //secondsRemaining, logo, logoTick, logoAction, callback for expiration actions and starting next phase
+function TimerPhase(chk, secondsRemaining, logo, action, textColor, endPhase) {
     this.chk = chk;
     this.secondsRemaining = secondsRemaining;
+    this.logoName = logo;
+    this.logoTick = logoHeight / secondsRemaining;
+    this.logoAction = action;
+    this.textColor = textColor;
     this.endPhase = endPhase;
-    var logoName = logo,
-        logoTickA = logoHeight / secondsRemaining,
-        logoAction = action;
 }
 
 function PomodoroTimer() {
-    this.runPhase = runPhase;
+    this.initPhases = initPhases;
     this.reset = reset;
+    this.pause = pause;
+    this.resume = resume;
+    this.isPaused = false;
+    this.isStarted = false;
 
-    var inPomodoro,
-        timeRemaining,
-        rep,
-        pomSeconds,
-        shortSeconds,
-        longSeconds,
-        logoTick,
-        tickInterval,
-        logoPomTick,
-        logoShortTick,
-        logoLongTick;
-
+    var tickInterval;
     var phases = [];
 
-    function initPhases() {
-        //timeRemaining, logo, logoTick, logoAction, callback for expiration actions and starting next phase
+    function initPhases() { //should this be a static TimerPhase function that returns the phases object?
+        console.log("initPhases");
+        var newPhase;
+        let chk = 1;
         for (let i = 0; i < 8; i++) {
             if (i % 2 == 0) {
-                let newPhase = new TimerPhase(i + 1, $("#pom-dur").val() * 60, "#logo-black", "dec", endPom);
+                newPhase = new TimerPhase(chk, $("#pom-dur").val() * 60, "#logo-black", " - ", "#00AA0E", endPom);
+                chk++;
             } else {
                 let remaining = i != 7 ? $("#sht-rest-dur").val() * 60 : $("#lng-rest-dur").val() * 60;
-                let newPhase = new TimerPhase(i + 1, remaining, "#logo-blue", "inc", endRest);
+                newPhase = new TimerPhase(chk - 1, remaining, "#logo-blue", " + ", "#0000FF", endRest);
             }
             phases.push(newPhase);
         }
-    }
-
-    function getPhase() {
-        let curPhase = phases[0];
-        tickInterval = setInterval(tick, 1000, curPhase);
+        console.dir(phases);
+        getPhase();
     }
 
     function endPom() {
+        console.log("ending Pomodoro");
         let chkPhase = phases.shift();
         $("#chk-" + chkPhase.chk).attr("checked", true);
         getPhase();
     }
 
     function endRest() {
+        console.log("ending rest");
         $("#logo-black").height("358px");
         $("#logo-green").height("358px");
         $("#logo-blue").height("0"); //can I do something like change the opacity of blue and black in opp so it looks like I'm fading from blue to black?
@@ -107,93 +91,39 @@ function PomodoroTimer() {
         getPhase();
     }
 
-    function runPhase() {
-        if (!rep) {
-            rep = 1;
-            inPomodoro = true;
-            pomSeconds = $("#pom-dur").val() * 60;
-            shortSeconds = $("#sht-rest-dur").val() * 60;
-            longSeconds = $("#lng-rest-dur").val() * 60;
-            logoPomTick = logoHeight / pomSeconds;
-            logoShortTick = logoHeight / shortSeconds;
-            logoLongTick = logoHeight / longSeconds;
-        }
-
-        if (rep > 4) {
-            clearInterval(tickInterval);
-            //reset
-            return;
-        }
-
-        if (inPomodoro) {
-            timeRemaining = pomSeconds;
-            $("#timer").css("color", "#00AA0E");
-            // console.log($("#timer").css("color"));
-        } else {
-            $("#timer").css("color", "#0000FF");
-            // console.log($("#timer").css("color"));
-
-            if (rep < 4) {
-                timeRemaining = shortSeconds;
-            } else {
-                timeRemaining = longSeconds;
-            }
-        }
-
-        console.log("Running: rep " + rep + " " + (inPomodoro ? "Pomodoro" : "Rest"));
-        console.log("Time Remaining: " + timeRemaining);
-        tickInterval = setInterval(tick, 1000);
-        // tickInterval = setInterval(tick, 1000, runPhase);
+    function getPhase() {
+        if (phases.length == 0) return; // initPhases
+        let curPhase = phases[0];
+        $("#timer").css("color", curPhase.textColor);
+        tickInterval = setInterval(tick, 1000, curPhase);
     }
 
     function tick(phase) {
-        phase.secondsRemaining = phase.secondsRemaining--;
+        phase.secondsRemaining = phase.secondsRemaining - 1;
         $("#timer").text(new Date(1000 * phase.secondsRemaining).toISOString().substr(14, 5));
-        let newHeight = eval($(phase.logo).height() + phase.logoAction + "60");
-        console.log("newHeight: " + newHeight);
-        $(phase.logo).height(newHeight);
+        let newHeight = eval($(phase.logoName).height() + phase.logoAction + phase.logoTick);
+        $(phase.logoName).height(newHeight);
 
-        if (secondsRemaining == 0) {
+        if (phase.secondsRemaining == 0) {
+            clearInterval(tickInterval);
+            console.log("endPhase");
             phase.endPhase();
         }
     }
-    /*     function tick() {
-            //pass phase into tick and reference the properties directly. It removes the need for globals
-            //is a callback necessary here? when would this callback (runPhase) be called if I referenced directly at the same point in the method
-            timeRemaining--;
-            $("#timer").text(new Date(1000 * timeRemaining).toISOString().substr(14, 5));
-            console.log("timeRemaining: " + timeRemaining);
-            if (inPomodoro) {
-                $("#logo-black").height($("#logo-black").height() - logoPomTick);
-            } else {
-                if (rep < 4) {
-                    $("#logo-blue").height($("#logo-blue").height() + logoShortTick);
-                } else {
-                    $("#logo-blue").height($("#logo-blue").height() + logoLongTick);
-                }
-            }
 
-            if (timeRemaining == 0) {
-                clearInterval(tickInterval);
-                if (!inPomodoro) {
-                    rep++;
-                    $("#logo-black").height("358px");
-                    $("#logo-green").height("358px");
-                    $("#logo-blue").height("0"); //can I do something like change the opacity of blue and black in opp so it looks like I'm fading from blue to black?
-                } else {
-                    $("#chk-" + rep).attr("checked", true);
-                }
-                inPomodoro = !inPomodoro;
-                runPhase();
-            }
-        } */
+    function pause() {
+        clearInterval(tickInterval);
+        this.isPaused = true;
+    }
+
+    function resume() {
+        this.isPaused = false;
+        getPhase();
+    }
 
     function reset() {
         clearInterval(tickInterval);
         //should I just nullify timer rather than the vars?
-        rep = null;
-        inPomodoro = null;
-        timeRemaining = null;
         $("#time").css("color", "black");
 
     }
